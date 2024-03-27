dd90c8e31dc2ad61b106a2f4f4d220d9f79c0803
Rodrigo Jovê <103905807+jovepy@users.noreply.github.com>
2024-03-27 13:59:34 -0300

    portfolio zerado

--- principal/urls.py
+++ principal/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", principal.as_view(),name='inicio'),
+    path("", principal.as_view(template='inicio.html'),name='inicio'),
     path('toggle-theme/', toggle_theme, name='toggle-theme'),
 
 ]
--- principal/views.py
+++ principal/views.py
@@ -5,30 +5,25 @@ import json
 
 def toggle_theme(request):
     current_theme = request.COOKIES.get('theme', 'dark')
-    new_theme = 'dark' if current_theme == 'light' else 'light'
-        
-    # Obtém a URL referer (página anterior) ou redireciona para uma página padrão
-    referer = request.META.get('HTTP_REFERER') or '/'
-    
+    new_theme = 'dark' if current_theme == 'light' else 'light'    
+    referer = request.META.get('HTTP_REFERER') or '/'    
     response = redirect(referer)
     response.set_cookie('theme', new_theme)
     return response
 
 def saudacao_com_base_no_horario():
-    agora = datetime.now().time()
-    
+    agora = datetime.now().time()    
     if datetime.strptime('04:30:00', '%H:%M:%S').time() <= agora < datetime.strptime('12:00:00', '%H:%M:%S').time():
         saudacao = "Bom dia"
     elif datetime.strptime('12:00:00', '%H:%M:%S').time() <= agora <= datetime.strptime('18:00:00', '%H:%M:%S').time():
         saudacao = "Boa tarde"
     else:
         saudacao = "Boa noite"
-    
     return saudacao
 
 class principal(View):
-    
-    def get(self, request):
+    template = 'inicio.html'
+    def get(self, request, *args, **kwargs):        
         self.context = {}    
         self.context['saudacao'] = saudacao_com_base_no_horario()
-        return render (request,'inicio.html',self.context)
+        return render (request, self.template ,self.context)
