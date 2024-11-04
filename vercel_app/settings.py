70c5c0161e01601ec4fbaebe7f82e8db1d81c0a5
Rodrigo Jovê <103905807+jovepy@users.noreply.github.com>
2024-11-04 13:19:19 -0300

    particular corrigidas

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!DEBUG = False
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','www.jovepy.com.br']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app',os.environ.get('SITE') ]
 
 
 # Application definition
@@ -132,11 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/estaticos/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'estaticos')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "styles")
+    os.path.join(BASE_DIR, "static")
 ]
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
