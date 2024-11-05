1b88b809b767625eb2059646b10a36377b5f6bd8
Rodrigo Jovê <103905807+jovepy@users.noreply.github.com>
2024-11-04 22:47:10 -0300

    particulas

--- staticfiles/particles/particles.js
+++ staticfiles/particles/particles.js
@@ -20,7 +20,7 @@ var pJS = function(tag_id, params){
     },
     particles: {
       number: {
-        value: 200,
+        value: 100,
         density: {
           enable: true,
           value_area: 800
@@ -59,7 +59,7 @@ var pJS = function(tag_id, params){
         random: false,
         anim: {
           enable: false,
-          speed: 20,
+          speed: 10,
           size_min: 0,
           sync: false
         }
