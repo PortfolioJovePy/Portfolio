c4a91e6799c0f5d7059b635a99160e9fd37a8bf8
Rodrigo Jovê <103905807+jovepy@users.noreply.github.com>
2024-12-16 14:33:50 -0300

    sub objetivo

--- metricas/models.py
+++ metricas/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Metasdelongoprazo(models.Model):    
-    nome = models.CharField(max_length=140)    
+    nome = models.CharField(max_length=140)  
+    def __str__(self):
+        return self.nome
+  
     
     
 class Objetivosmarco(models.Model):
@@ -11,9 +14,14 @@ class Objetivosmarco(models.Model):
     def __str__(self):
         return self.nome
 
+class Microobjetivos(models.Model):
+    nome_objetivomarco = models.ForeignKey(Objetivosmarco, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=140)    
+    def __str__(self):
+        return self.nome
 
 class Computarevolucao(models.Model):
-    objetivo = models.ForeignKey(Objetivosmarco, on_delete=models.CASCADE)
+    objetivo = models.ForeignKey(Microobjetivos, on_delete=models.CASCADE)
     nota = models.IntegerField(choices=[(i, str(i)) for i in range(1, 101)], help_text="Nota de 1 a 100")
     data = models.DateTimeField(auto_now_add=True)
 
