b61b85dece56c0364dcc80291f9ef4f0b9ab8881
Rodrigo Jovê <103905807+jovepy@users.noreply.github.com>
2025-03-18 13:28:40 -0300

    ajuste de hoje so

--- emails/urls.py
+++ emails/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 urlpatterns = [
     
-    path("painel-geral/", gerenciador.as_view(template='painel.html'),name='painel_email'),
+    path("", gerenciador.as_view(template='painel.html'),name='painel_email'),
     path('crm/',gerenciador.as_view(template='CRM.html'), name='crm'),
     path("contatos/", gerenciador.as_view(template='contatos.html'),name='contatos'),
     path("agendamento/", gerenciador.as_view(template='agendamento.html'),name='agendamento'),
